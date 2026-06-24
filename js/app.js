3104bd2e2e7c0fda34397d05df5882f6cc36845a
mohamedelssiy-dev <mohamedelssiy@gmail.com>
2026-06-24 18:05:48 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,30 @@
-alert("APP LOADED");
+document.addEventListener("DOMContentLoaded", () => {
+
+    const cards = document.querySelectorAll(".feature-card");
+
+    cards.forEach(card => {
+
+        card.addEventListener("click", () => {
+
+            const title = card.querySelector("h3").innerText;
+
+            openSection(title);
+
+        });
+
+    });
+
+});
+
+function openSection(sectionName){
+
+    const app = document.getElementById("app");
+
+    app.innerHTML = `
+        <section class="hero">
+            <h1>${sectionName}</h1>
+            <p>Coming Soon</p>
+        </section>
+    `;
+
+}
