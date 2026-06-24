14a26f721c3d2b0ae3d1ea5a377e26558d6895aa
mohamedelssiy-dev <mohamedelssiy@gmail.com>
2026-06-24 17:26:16 +0300

    Create language.js

---
+++ js/language.js
@@ -0,0 +1,8 @@
+// Long Beach Concierge Languages
+
+let currentLanguage = "en";
+
+function changeLanguage(language) {
+    currentLanguage = language;
+    console.log("Language Changed:", language);
+}
